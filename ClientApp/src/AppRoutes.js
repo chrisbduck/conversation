8a661085f863e934a344ce1af42b68ef7c50f8b4
Chris Bevan <>
2023-07-24 14:08:55 -0700

    more TypeScript

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,24 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
-
-const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var Counter_1 = require("./components/Counter");
+var FetchData_1 = require("./components/FetchData");
+var Home_1 = require("./components/Home");
+var AppRoutes = [
+    {
+        index: true,
+        element: react_1.default.createElement(Home_1.Home, null)
+    },
+    {
+        path: '/counter',
+        element: react_1.default.createElement(Counter_1.Counter, null)
+    },
+    {
+        path: '/fetch-data',
+        element: react_1.default.createElement(FetchData_1.FetchData, null)
+    }
 ];
-
-export default AppRoutes;
+exports.default = AppRoutes;
