34b8b5d428521e189567175ab820627e5f121ff3
Chris Bevan <>
2023-07-24 16:35:23 -0700

    Partially working

--- ClientApp/src/components/CharacterChat.tsx
+++ ClientApp/src/components/CharacterChat.tsx
@@ -22,14 +22,37 @@ export class CharacterChat extends Component {
 
     return (
       <div>
-        <h1 id="tableLabel">ChatGPT's joke</h1>
+        <h1>Conversation</h1>
         {contents}
+        Say something: <input name="userQuery" /> <button type="button" onClick={this.updateChatData}>submit</button>
       </div>
     );
   }
 
   async populateChatData() {
     const response = await fetch('chat?query=hello');
+    const data = await response.json();
+    if (!response.ok) {
+      this.setState({ chatText: response.statusText, loading: false });
+      return;
+    }
+    this.setState({ chatText: data, loading: false });
+  }
+
+  async updateChatData() {
+    const chatHistory = {
+      "characterHistory": ["hello"],
+      "userHistory": ["goodbye"],
+    };
+    const historyText = JSON.stringify(chatHistory);
+    const response = await fetch('chat', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: historyText
+    });
     const data = await response.json();
     if (!response.ok) {
       this.setState({ chatText: response.statusText, loading: false });
--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OpenAI_API;
 using OpenAI_API.Chat;
 
 namespace webapi.Controllers;
+
+public class ChatHistory
+{
+    public string[]? CharacterHistory { get; set; }
+    public string[]? UserHistory { get; set; }
+};
 
 [ApiController]
 [Route("[controller]")]
@@ -17,19 +24,63 @@ public class ChatController : Controller
         _logger = logger;
     }
 
-    private async Task<string> GetExampleOutputAsync()
+    private static async Task<T?> GetStreamAsJSON<T>(Stream inputStream)
     {
+        string input;
+        using (var reader = new StreamReader(inputStream)) {
+            input = await reader.ReadToEndAsync();
+        }
+        return JsonConvert.DeserializeObject<T>(input);
+    }
+
+    private async Task<string> GetChatResponseAsync(Stream inputStream)
+    {
+        ChatHistory? history = await GetStreamAsJSON<ChatHistory>(inputStream);
+
         OpenAIAPI api = new(c_openaiKey);
 
         Conversation chat = api.Chat.CreateConversation();
-        chat.AppendUserInput("Tell me a random joke!");
+        chat.AppendUserInput(
+            @"I want you do act like you're a character from an interactive medieval fantasy story.  Do not break character for any reason.
+
+            Your back story is that you're a dwarven blacksmith, 35 years old, with a wife and two small children.  You are a master fighter with a battle axe,
+            and are familiar with all other weapons.  Your name is Grolf, and you talk with a Scottish accent.
+
+            The scenario is that a human warrior has walked up to you and said hello.  Please respond and carry on a conversation from there."
+        );
+        chat.AppendExampleChatbotOutput("Ah, greetings and good day to ye, laddie!");
+        chat.AppendUserInput("What weapons are on sale today?");
+        /*if (history != null)
+        {
+            int characterIndex = 0;
+            int userIndex = 0;
+            if (history.CharacterHistory == null)
+                throw new InvalidOperationException("Invalid character history");
+            if (history.UserHistory == null)
+                throw new InvalidOperationException("Invalid user history");
+            while (characterIndex < history.CharacterHistory.Length || userIndex < history.UserHistory.Length)
+            {
+                if (characterIndex < history.CharacterHistory.Length)
+                    chat.AppendExampleChatbotOutput(history.CharacterHistory[characterIndex++]);
+                if (userIndex < history.UserHistory.Length)
+                    chat.AppendUserInput(history.UserHistory[userIndex++]);
+            }
+        }*/
         return await chat.GetResponseFromChatbotAsync();
     }
 
-    [HttpGet(Name = "GetCompletion")]
-    public JsonResult Get(string query)
+    [HttpGet()]
+    public JsonResult Get()
     {
-        string output = Task.Run(GetExampleOutputAsync).Result;
+        string output = Task.Run(() => GetChatResponseAsync(HttpContext.Request.Body)).Result;
+        Console.WriteLine(output);
+        return new JsonResult(output);
+    }
+
+    [HttpPost()]
+    public JsonResult Post()
+    {
+        string output = Task.Run(() => GetChatResponseAsync(HttpContext.Request.Body)).Result;
         Console.WriteLine(output);
         return new JsonResult(output);
     }
