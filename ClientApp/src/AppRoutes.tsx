08e4f9367d70b8c1dc93f111ebcf392a1600a8c7
Chris Bevan <>
2023-07-31 10:04:00 -0700

    Fixed name; removed unneeded stuff

--- ClientApp/src/AppRoutes.tsx
+++ ClientApp/src/AppRoutes.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { CharacterChat } from "./components/CharacterChat";
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
   },
   {
     path: '/grolf-chat',
--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CharacterConversation2.Pages
+namespace CharacterConversation.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
